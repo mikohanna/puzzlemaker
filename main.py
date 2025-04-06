f8f0974352a0de2cc335711249076c4092855791
Hanna Miko <mikolaurahanna@gmail.com>
2025-04-06 23:01:33 +0200

    minor reformatting in main.py

--- main.py
+++ main.py
@@ -4,59 +4,39 @@ import binairo_generator as binairo
 
 app = Flask(__name__)
 
-@app.route('/generate', methods=['GET'])
+@app.route('/sample', methods=['GET'])
 def generate_json():
-    # Example JSON data
     data = {"puzzle": [[1, 2, 3], [4, 5, 6]]}
-    
-    return jsonify(data)  # Sends JSON response
+    return jsonify(data)
 
 @app.route('/tents_easy', methods=['GET'])
 def generate_tents_easy():
-    # Example JSON data
-    data = tents.generate_tents_grid(difficulty=1)
-    
-    return jsonify(data)  # Sends JSON response
+    return jsonify(tents.generate_tents_grid(difficulty=1))
 
 
 @app.route('/tents_medium', methods=['GET'])
 def generate_tents_medium():
-    # Example JSON data
-    data = tents.generate_tents_grid(difficulty=2)
-    
-    return jsonify(data)  # Sends JSON response
+    return jsonify(tents.generate_tents_grid(difficulty=2))
 
 
 @app.route('/tents_hard', methods=['GET'])
 def generate_tents_hard():
-    # Example JSON data
-    data = tents.generate_tents_grid(difficulty=3)
-    
-    return jsonify(data)  # Sends JSON response
+    return jsonify(tents.generate_tents_grid(difficulty=3))
 
 
 @app.route('/binairo_easy', methods=['GET'])
 def generate_binairo_easy():
-    # Example JSON data
-    data = binairo.generate_binairo_puzzle(difficulty=1)
-    
-    return jsonify(data)  # Sends JSON response
+    return jsonify(binairo.generate_binairo_puzzle(difficulty=1))
 
 
 @app.route('/binairo_medium', methods=['GET'])
 def generate_binairo_medium():
-    # Example JSON data
-    data = binairo.generate_binairo_puzzle(difficulty=2)
-    
-    return jsonify(data)  # Sends JSON response
+    return jsonify(binairo.generate_binairo_puzzle(difficulty=2))
 
 
 @app.route('/binairo_hard', methods=['GET'])
 def generate_binairo_hard():
-    # Example JSON data
-    data = binairo.generate_binairo_puzzle(difficulty=3)
-    
-    return jsonify(data)  # Sends JSON response
+    return jsonify(binairo.generate_binairo_puzzle(difficulty=3))
 
 
 
